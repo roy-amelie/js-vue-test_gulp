1268d9d81dc4e9fec768702baca031854b1d5de6
roy-amelie <roydorelon.amelie@gmail.com>
2020-10-13 16:16:30 +0200

    autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
+const autoprefixer = require('gulp-autoprefixer');
+
 
 function defaultTask(cb) {
-    gulp.src('images/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('build/images'))
-    cb();
+    // gulp.src('images/*')
+    //     .pipe(imagemin())
+    //     .pipe(gulp.dest('build/images'))
+    
+    gulp.src('style.css')
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(gulp.dest('build'))
+        cb();
   }
   
   exports.default = defaultTask
